ad5af099fdc195b9784529f80056ddbc23d21ad1
CheckToBeSureApp <ufrc28@gmail.com>
2024-12-19 20:49:26 +0200

    init

---
+++ flask_app.py
@@ -0,0 +1,80 @@
+from flask import Flask, request, render_template
+from flask_cors import CORS
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+CORS(app)
+
+
+
+@app.route('/', methods=['GET']) # end point 
+def home():
+        return render_template('Homepage.html')
+
+@app.route('/search', methods=['GET'])
+def search():
+     return render_template('Search.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    data = request.form.get('formData')
+
+    if data:
+        data_list = data.split(',')
+
+        age_category = int(data_list[0])
+        gender = int(data_list[1])
+        duration_days = int(data_list[2])
+        symptoms_values = [int(i) for i in data_list[3:]]
+
+        response = [age_category, gender, duration_days] + symptoms_values
+        response = np.array(response).reshape(1, -1)  # Reshape to 2D array
+
+        # Load the model
+        with open("C:\\Users\\m\\Desktop\\Medical\\model_test_two.pkl", 'rb') as file:
+                model = pickle.load(file)
+                print("Model loaded successfully. Type:", type(model))
+                prediction = model.predict(response)
+                prediction = prediction.tolist()[0]
+
+        disease_dict = {
+                         "hypertension": ("Lisinopril", "Zestril"),
+                         "bronchitis": ("Albuterol", "Ventolin"),
+                         "migraine": ("Sumatriptan", "Imitrex"),
+                         "gastritis": ("Omeprazole", "Prilosec"),
+                         "allergy": ("Loratadine", "Claritin"),
+                         "pneumonia": ("Amoxicillin", "Augmentin"),
+                         "asthma": ("Fluticasone", "Flovent"),
+                         "eczema": ("Cetirizine", "Zyrtec"),
+                         "food poisoning": ("Loperamide", "Imodium"),
+                         "diabetes": ("Metformin", "Glucophage"),
+                         "depression": ("Sertraline", "Zoloft"),
+                         "anxiety": ("Diazepam", "Valium"),
+                         "sinusitis": ("Fluticasone", "Fluticasone"),
+                         "covid-19": ("Remdesivir", "Remdesivir"),
+                         "common cold": ("Fexofenadine", "Allegra"),
+                         "flu": ("Oseltamivir", "Tamiflu")
+                    }
+
+        return f"Disease:  {prediction} \nActive Ingredient:  {disease_dict[str(prediction).lower()][0]} \nMedicine: {disease_dict[str(prediction).lower()][1]}", 200
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+#     with open("C:\\Users\\m\\Desktop\\Price Comparison\\Price Comparison\\project\\KNN.pkl", 'rb') as file:
+#         model = pickle.load(file)
+#     with open('C:\\Users\\m\\Desktop\\Price Comparison\\Price Comparison\\project\\vectorizer.pkl', 'rb') as file2:
+#         loaded_vectorizer  = pickle.load(file2)
+
+#     with open('C:\\Users\\m\\Desktop\\Price Comparison\\Price Comparison\\project\\mlb.pkl', 'rb') as file3:
+#         mlb  = pickle.load(file3)
+
+#     result = model.predict(loaded_vectorizer.transform([query]))
+#     result = mlb.inverse_transform(result)
+#     print(result)
---
+++ static/js/PWA.js
@@ -0,0 +1,42 @@
+﻿// Service worker file: sw.js
+
+const cacheName = 'image-cache-v1';
+const resourcesToCache = [
+    '/static/Images/FemaleDocktorNew.jpg',
+    '/static/Images/hStat.jpg',
+    '/static/Images/OnlineHelpeNew.jpg'
+];
+
+// Install service worker and cache the images
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(cacheName).then(cache => {
+            return cache.addAll(resourcesToCache);
+        })
+    );
+});
+
+// Fetch the images from cache if available, otherwise fetch from network
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+// Activate service worker and clear old caches if needed
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [cacheName];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (!cacheWhitelist.includes(cache)) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
